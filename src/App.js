fefa4ade6f8ed40fdfc8ed72ef6c5fa75d7e9b8d
Rashed Khan <abir97bd@gmail.com>
2020-10-05 07:52:16 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
+  let greeting = "";
+  const cssStyle = {};
+
+  let curTime = new Date().getHours().toLocaleString();
+
+  if (curTime >= 1 && curTime < 12) {
+    greeting = "Good Morning";
+    cssStyle.color = "green";
+  } else if (curTime >= 12 && curTime < 19) {
+    greeting = "Good Afternoon";
+    cssStyle.color = "yellow";
+  } else {
+    greeting = "Good Night";
+    cssStyle.color = "red";
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="heading">
+      <h1>hello sir, <span style={cssStyle}>{greeting}</span> </h1>
     </div>
   );
 }
